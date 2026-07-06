2a4f419b31dd6eee9b2a387a64f09d7393830af7
mikrowellentoast <105505450+mikrowellentoast@users.noreply.github.com>
2026-07-06 13:45:31 +0200

    Initial commit

---
+++ src/main/java/org/mikrowellentoast/excavator/Excavator.java
@@ -0,0 +1,16 @@
+package org.mikrowellentoast.excavator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Excavator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
