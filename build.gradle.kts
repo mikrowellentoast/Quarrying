d2e734952fb1a46c758270f33e4624c1a1afd34d
mikrowellentoast <105505450+mikrowellentoast@users.noreply.github.com>
2026-07-11 20:13:34 +0200

    support for 1.21.5-1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
 tasks {
